787af46928945706790e16fdab14f3d0227d471c
Bilal <bilalallawala@northwesternmutual.com>
2019-06-09 12:18:28 -0400

    udpates

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
+import 'src/facebookapp.dart';
+//
+//void main() {
+//  runApp(App());
+//}
 
-void main() {
-  runApp(App());
-}
+void main() => runApp(new MyApp());
